986b73d792e61abc84ff779b5703e80bc677731c
maliang <924818949@qq.com>
2017-12-11 13:48:42 +0800

    基于SSM框架的简易的增删改查功能，通过maven整合

--- myfirst-mvc/MySQL/user.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `user` (`userid`, `username`, `password`, `email`) VALUES (121, '古天乐', '123', '123456789@qq.com');
-INSERT INTO `user` (`userid`, `username`, `password`, `email`) VALUES (122, '陈意涵', '123456', '991122334@qq.com');
-INSERT INTO `user` (`userid`, `username`, `password`, `email`) VALUES (123, '马亮南生', '123454', '123432123@gmail.com');
-INSERT INTO `user` (`userid`, `username`, `password`, `email`) VALUES (124, 'xiaoming', '45677', '878677656@qq.com');
-INSERT INTO `user` (`userid`, `username`, `password`, `email`) VALUES (125, '45667', 'gygygygy', '667788899@163.com');
-INSERT INTO `user` (`userid`, `username`, `password`, `email`) VALUES (126, '54545gygyhgy', 'uyuyuyuy88888', '66767767788@qq.com');
